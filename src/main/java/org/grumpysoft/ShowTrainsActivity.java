a2d9227497c73ffbb4d67af341ef7d1ddc64d87e
James Byatt <grumpyjames@gmail.com>
2015-12-07 18:58:39 +0000

    Dead code

--- src/main/java/org/grumpysoft/ShowTrainsActivity.java
+++ src/main/java/org/grumpysoft/ShowTrainsActivity.java
@@ -1,10 +1,8 @@
 package org.grumpysoft;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -42,66 +40,6 @@ public class ShowTrainsActivity extends Activity {
         super.onStop();
 
         Favourites.save(getPreferences(0));
-    }
-
-    private static class TrainTableRow extends TableRow {
-
-        private TextView destinationArrivalTimeView;
-
-        public TrainTableRow(Context context, DepartingTrain train) {
-            super(context);
-            setId(train.serviceId().hashCode()); //risky business!
-            setPadding(1, 1, 1, 1);
-            addStationNameView(train);
-            addPlatformView(train);
-            addScheduledAtView(train);
-            addExpectedAtView(train);
-            this.destinationArrivalTimeView = rightAlignedText();
-            addView(destinationArrivalTimeView);
-        }
-        
-        private void updateArrivalTime(String time) {
-            destinationArrivalTimeView.setText(time);
-        }
-
-        private void addExpectedAtView(DepartingTrain train) {
-            addRightGravityTextView(train.expectedAt());
-        }
-
-        private void addScheduledAtView(DepartingTrain train) {
-            addRightGravityTextView(train.scheduledAt());
-        }
-
-        private void addPlatformView(DepartingTrain train) {
-            addRightGravityTextView(train.platform());
-        }
-
-        private void addRightGravityTextView(String content) {
-            final TextView view = rightAlignedText();
-            view.setText(content);
-            addView(view);
-        }
-
-        private TextView rightAlignedText() {
-            final TextView view = textViewWithGravity(Gravity.RIGHT);
-            view.setPadding(0, 0, 3, 0);
-            return view;
-        }
-
-        private void addStationNameView(DepartingTrain train) {
-            final TextView view = textViewWithGravity(Gravity.LEFT);
-            view.setGravity(Gravity.LEFT);
-            view.setPadding(3, 0, 3, 0);
-            view.setText(Joiner.on(", ").join(train.destinationList()));
-            addView(view);
-        }
-
-        private TextView textViewWithGravity(int gravity) {
-            final TextView view = new TextView(getContext());
-            view.setGravity(gravity);
-            Utility.changeFonts(view, getContext().getAssets(), getContext().getResources());
-            return view;
-        }
     }
 
     private class FetchDetailsTask extends AsyncTask<DepartingTrain, Integer, ServiceDetailsOrError> {
