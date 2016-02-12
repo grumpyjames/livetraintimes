d918450e4dee24e5804803fb464dbda73d0b08be
James Byatt <grumpyjames@gmail.com>
2016-02-12 22:26:04 +0000

    apparently this can also be null, ffs

--- src/main/java/net/digihippo/soap/SoapLiveTrainsService.java
+++ src/main/java/net/digihippo/soap/SoapLiveTrainsService.java
@@ -63,7 +63,7 @@ public class SoapLiveTrainsService implements DepartureBoardService {
         // On time => scheduled time = estimated time
         // Delayed or Cancelled => exactly that
         // Otherwise, etd is actually a usable time string.
-        if (etd.equals("On time")) {
+        if ("On time".equals(etd)) {
             return Either.right(std);
         }
         for (BadTrainState badTrainState: BadTrainState.values()) {
